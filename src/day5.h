1c1adc86dced5811a9942940330b63b92a003aa9
Alex Blackie <alex@blackie.me>
2022-12-06 10:21:44 -0800

    Add tests for crate movement functions
    We need tests for the functions that manipulate the crates array. We
    should pull in the test input from AoC's instructions since it is
    simpler and smaller and easier to deal with.

--- src/day5.h
+++ src/day5.h
@@ -18,4 +18,9 @@ typedef struct CrateMovement
 } CrateMovement;
 
 CrateMovement* crates_parse_move(char *src);
+void crates_move(Crate *crates[], CrateMovement *m);
+void crates_ordered_move(Crate *crates[], CrateMovement *m);
+void crates_parse_stacks(FILE *fp, Crate *crates[]);
+void crates_free(Crate *crates[], size_t size);
+void crates_debug_stacks(Crate *crates[], size_t size);
 int day5(char result[9], bool ordered);
--- test/test_day5.c
+++ test/test_day5.c
@@ -1,6 +1,18 @@
 #include "../src/day5.h"
 #include "greatest/greatest.h"
 
+void _crates_fixture(Crate *crates[])
+{
+	FILE *fp = fopen("test/fixtures/day5.txt", "r");
+	if (fp == NULL)
+	{
+		printf("Failed to open fixture!");
+		return;
+	}
+	crates_parse_stacks(fp, crates);
+	fclose(fp);
+}
+
 TEST test_crates_parse_move()
 {
 	char *cmd = "move 4 from 6 to 1";
@@ -15,7 +27,41 @@ TEST test_crates_parse_move()
 	PASS();
 }
 
+TEST test_crates_move()
+{
+	Crate *crates[3] = {NULL, NULL, NULL};
+	_crates_fixture(crates);
+
+	CrateMovement *move = crates_parse_move("move 2 from 2 to 1");
+	crates_move(crates, move);
+	free(move);
+
+	ASSERT_EQ_FMT('C', crates[0]->name, "%c");
+	ASSERT_EQ_FMT('D', crates[0]->next->name, "%c");
+
+	crates_free(crates, 3);
+	PASS();
+}
+
+TEST test_crates_ordered_move()
+{
+	Crate *crates[3] = {NULL, NULL, NULL};
+	_crates_fixture(crates);
+
+	CrateMovement *move = crates_parse_move("move 2 from 2 to 1");
+	crates_ordered_move(crates, move);
+	free(move);
+
+	ASSERT_EQ_FMT('D', crates[0]->name, "%c");
+	ASSERT_EQ_FMT('C', crates[0]->next->name, "%c");
+
+	crates_free(crates, 3);
+	PASS();
+}
+
 SUITE(day5_suite)
 {
 	RUN_TEST(test_crates_parse_move);
+	RUN_TEST(test_crates_move);
+	RUN_TEST(test_crates_ordered_move);
 }
